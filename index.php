2d6a2177aeee18b5aa6d80eea2b58ea26775c59c
AURESANTA <auredcsanta@gmail.com>
2019-11-03 22:52:45 +0100

    index rewrited with switch

--- index.php
+++ index.php
@@ -2,95 +2,102 @@
 require('controller/mainController.php');
 
 if (isset($_GET['action'])) {
-    if ($_GET['action'] == 'showPosts') {
+    $action = $_REQUEST['action'];
+}
+else {
+    $action = 'showPosts';
+}
 
-        showPosts();
-    }
-    elseif ($_GET['action'] == 'showPost') {
-        if (isset($_GET['id']) && $_GET['id'] > 0) {
+        switch($action) { 
+            case 'showPosts': 
+                showPosts();
+            break;
 
-            showPost();
-        }
-        else {
-            echo 'Erreur : Article introuvable';
-        }
-    }
+            case 'showPost':
+            if (isset($_GET['id']) && $_GET['id'] > 0) {
 
-    elseif($_GET['action'] == 'addPost') {
-        if (!empty($_SESSION['username']) && !empty($_POST['content'])&& !empty($_POST['title'])){
-            $imgUrl = getImgUrl(); 
-            addPost($_POST['title'], $_SESSION['username'],  $_POST['content'],  $imgUrl, $_SESSION['id'], $_POST['category']); 
-        }
-        else {
-            echo 'Erreur : tous les champs ne sont pas remplis !';
-        }
-    }
+                showPost();
+            }
+            else {
+                echo 'Erreur : Article introuvable';
+            }
+            break;
 
-    elseif ($_GET['action'] == 'updatePost') {
-        if (isset($_GET['id']) && $_GET['id'] > 0) {
-            $imgUrl = getImgUrl(); 
-            updatePost($_POST['title'], $_POST['content'],  $imgUrl, $_POST['category'], $_GET['id'], );
-        }
-    }
-
-    elseif ($_GET['action'] == 'deletePost') {
-        if (isset($_GET['id']) && $_GET['id'] > 0) {
-
-            deletePost();
-        }
-    }
-
-    elseif ($_GET['action'] == 'addComment') {
-        if (isset($_GET['id']) && $_GET['id'] > 0) {
-            if (!empty($_POST['content'])) {
-                addComment($_GET['id'], $_SESSION['username'], $_POST['content']);
+            case 'addPost':
+            if (!empty($_SESSION['username']) && !empty($_POST['content'])&& !empty($_POST['title'])){
+                $imgUrl = getImgUrl(); 
+                addPost($_POST['title'], $_SESSION['username'],  $_POST['content'],  $imgUrl, $_SESSION['id'], $_POST['category']); 
             }
             else {
                 echo 'Erreur : tous les champs ne sont pas remplis !';
             }
-        }
-        else {
-            echo 'Erreur : aucun identifiant de billet envoyé';
-        }
-    }
+            break;
 
-    elseif ($_GET['action'] == 'deleteComment') {
-        if (isset($_GET['id']) && $_GET['id'] > 0) {
-            deleteComment($_GET['id']);
-        }
-    }
-
-    elseif ($_GET['action'] == 'showLogin') {
-        showLogin();
-    }
-
-    elseif ($_GET['action'] == 'showUser') {
-        showUserPosts($_SESSION['id']);
-    }
-
-    elseif ($_GET['action'] == 'showUpdate') {
-        showUpdate($_SESSION['id'], $_GET['id']);
-    }
-
-
-    elseif ($_GET['action'] == 'disconnectUser') {
-        disconnectUser();
-
-    }
-    elseif ($_GET['action'] == 'newUser') {
-        if (!empty($_POST['username']) && !empty($_POST['password'])) {
-            newUser($_POST['username'], $_POST['password']);
-        }
-    }
-    elseif ($_GET['action'] == 'verifyUser') {
-            if (!empty($_POST['username']) && !empty($_POST['password'])) {
-                verifyUser($_POST['username'], $_POST['password']);
+            case 'updatePost':
+            if (isset($_GET['id']) && $_GET['id'] > 0) {
+                $imgUrl = getImgUrl(); 
+                updatePost($_POST['title'], $_POST['content'],  $imgUrl, $_POST['category'], $_GET['id'], );
             }
-    }
+            break;
+
+            case 'deletePost':
+            if (isset($_GET['id']) && $_GET['id'] > 0) {
+
+                deletePost();
+            }
+            break;
+
+            case 'addComment':
+            if (isset($_GET['id']) && $_GET['id'] > 0) {
+                if (!empty($_POST['content'])) {
+                    addComment($_GET['id'], $_SESSION['username'], $_POST['content']);
+                }
+                else {
+                    echo 'Erreur : tous les champs ne sont pas remplis !';
+                }
+            }
+            else {
+                echo 'Erreur : aucun identifiant de billet envoyé';
+            }
+            break;
+
+            case 'deleteComment':
+            if (isset($_GET['id']) && $_GET['id'] > 0) {
+                deleteComment($_GET['id']);
+            }
+            break;
+
+            case 'showLogin':
+            showLogin();
+            break;
+
+            case 'showUser':
+            showUserPosts($_SESSION['id']);
+            break;
+
+            case 'showUpdate':
+            showUpdate($_SESSION['id'], $_GET['id']);
+            break;
+
+            case 'disconnectUser':
+            disconnectUser();
+            break;
+
+            case 'newUser':
+            if (!empty($_POST['username']) && !empty($_POST['password'])) {
+                newUser($_POST['username'], $_POST['password']);
+            }
+            break;
+
+            case 'verifyUser':
+                if (!empty($_POST['username']) && !empty($_POST['password'])) {
+                    verifyUser($_POST['username'], $_POST['password']);
+                }
+                break;
+
+            default:
+
+            showPosts();
 }
 
 
-
-else {
-    showPosts();
-}
